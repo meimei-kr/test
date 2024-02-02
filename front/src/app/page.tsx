33c67815668e137bbf34e9d7660a5da591038179
meimei-kr <77828683+meimei-kr@users.noreply.github.com>
2024-02-02 15:49:28 +0900

    update: サンプルアプリデータ取得時のメソッドとヘッダーを追加

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -7,7 +7,12 @@ type Post = {
 };
 
 const getAllPosts = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/posts`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/posts`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
   return res.json();
 }
 
